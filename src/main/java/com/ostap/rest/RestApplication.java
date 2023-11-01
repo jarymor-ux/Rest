6e47cd532545200561c6149d6ad9868e76fb5292
jarymor-ux <120585847+jarymor-ux@users.noreply.github.com>
2023-11-01 22:24:30 +0300

    .

--- src/main/java/com/ostap/rest/RestApplication.java
+++ src/main/java/com/ostap/rest/RestApplication.java
@@ -1,4 +1,4 @@
-package com.ostap.rest;
+    package com.ostap.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/ostap/rest/controllers/MainController.java
+++ src/main/java/com/ostap/rest/controllers/MainController.java
@@ -15,45 +15,36 @@ import com.ostap.rest.entity.User;
 import com.ostap.rest.repository.ProductRepository;
 import com.ostap.rest.repository.UserRepository;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/api")
-@Api(tags = "API")
 public class MainController {
     @Autowired
     private ProductRepository productRepository;
     @Autowired
     private UserRepository userRepository;
 
-
     @GetMapping("/getAllProducts")
-    @ApiOperation("Get all products in db")
     public List<Product> getAllProduct(){
         return productRepository.findAll();
     }
     
     @PostMapping("/addNewProduct")
-    @ApiOperation("Add 1 product to the db using JSON post query")
     public Product addProduct(@RequestBody Product product){
         return productRepository.save(product);
     }
 
     @GetMapping("/getUsers")
-    @ApiOperation("Get all users in db")
     public List<User> getAllUsers(){
         return userRepository.findAll();
     }
 
     @GetMapping("/getUsers/{id}")
-    @ApiOperation("Get 1 user from the db")
     public User getUserById(@PathVariable int id){
         return userRepository.findById(id).orElse(null);
     }
 
     @PostMapping("/addNewUser")
-    @ApiOperation("Add 1 User to the db using JSON post query")
     public User addNewUser(@RequestBody User user){
         return userRepository.save(user);
     }
@@ -63,7 +54,6 @@ public class MainController {
         User user = userRepository.getById(id);
         addProduct(product);
         product.setUser(user);
-
         return productRepository.save(product);
     }
 
